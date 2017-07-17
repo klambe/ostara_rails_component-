34d4305e97d2acd522f305d36c1a650c4e19a75e
kevin lambe <kevin.lambe@teamaolcom>
2017-07-17 13:32:54 +0100

    added faker gem and updated seeds.rb

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -1,3 +1,4 @@
 class MediaFile < ApplicationRecord
+  validates :filename, presence: true
   belongs_to :Event
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Event.create(category:'Site Events', sub_category1:'Christmas',  year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Christmas',  year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Thanksgiving', year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Bring Kids to Work', year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Interns', year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Corporate Events', year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Meet Ups', year:'2016', approval:true)
+# Event.create(category:'Site Events', sub_category1:'Halloween', year:'2016', approval:true)
+# Event.create(category:'Everyday Office', sub_category1:'Games Room', year:'2016', approval:true)
+# Event.create(category:'Everyday Office', sub_category1:'Theming', year:'2016', approval:true)
+# Event.create(category:'Everyday Office', sub_category1:'Birthdays', year:'2016', approval:true)
+# Event.create(category:'Culture & CAs', sub_category1:'Beer Bashes', year:'2016', approval:true)
+# Event.create(category:'Culture & CAs', sub_category1:'Sports', year:'2016', approval:true)
+# Event.create(category:'Culture & CAs', sub_category1:'Games', year:'2016', approval:true)
+# Event.create(category:'Culture & CAs', sub_category1:'Quizzes', year:'2016', approval:true)
+# Event.create(category:'Culture & CAs', sub_category1:'Photo Competition', year:'2016', approval:true)
+# Event.create(category:'Volunteering & CSR', sub_category1:'Charity Work', year:'2016', approval:true)
+# Event.create(category:'Volunteering & CSR', sub_category1:'Monster Help Day', year:'2016', approval:true)
+# Event.create(category:'Sports & Social', sub_category1:'Weekends Away', year:'2016', approval:true)
+# Event.create(category:'Sports & Social', sub_category1:'Food Events', year:'2016', approval:true)
+# Event.create(category:'Sports & Social', sub_category1:'Sports & Activities', year:'2016', approval:true)
+# Event.create(category:'Sports & Social', sub_category1:'Misc.', year:'2016', approval:true)
+
+5.times do
+  Event.create({
+                   title: Faker::Book.title,
+                   category: Faker::Book.genre,
+                   sub_category1: Faker::Book.genre,
+                   year: Faker::Lorem.words,
+                   month:Faker::Lorem.words,
+                   approval: true,
+
+               })
+end
