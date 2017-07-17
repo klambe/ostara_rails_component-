c35332ddd21e8b140c676b11094140a2b2d6ac1a
klambe17@gmail.com <klambe17@gmail.com>
2017-07-17 19:11:51 +0100

    added delete and find by id to rails app

--- app/controllers/v1/media_files_controller.rb
+++ app/controllers/v1/media_files_controller.rb
@@ -4,4 +4,25 @@ class V1::MediaFilesController < ApplicationController
     @media_files = MediaFile.all
     render json: @media_files, status: :ok
   end
+
+  def show
+    begin
+      @media_file = MediaFile.find(params[:id])
+      render json: @media_file, status: :ok
+    rescue ActiveRecord::RecordNotFound =>err
+      @response = {error: "doesn't exist"}
+      render 'generic/response.json.jbuilder',status: :not_found
+
+    end
+  end
+
+  def destroy
+    @media_file = MediaFile.where(id: params[:id]).first
+    if @media_file.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
 end
